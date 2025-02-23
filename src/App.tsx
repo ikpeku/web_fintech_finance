500083cf7f015606b1b6b26d73be79a92fd463d9
ikpeku <ikpedaniel07@gmail.com>
2025-02-23 03:03:58 +0100

    project initialize a header and footer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Footer, Header } from "./sections"
+
+function App() {
+
+  // max-w-screen-xl
+  return (
+    <main className="max-w-screen-2xl mx-auto">
+      <Header />
+      <Footer />
+    </main>
+   
+  )
+}
+
+export default App
